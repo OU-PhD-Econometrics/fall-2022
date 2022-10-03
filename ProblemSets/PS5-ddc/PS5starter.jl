88e62ca0dd5f0bd4cdec4a0ff97f005d7acaf1ab
Tyler Ransom <tyleransom@gmail.com>
2022-10-03 16:37:39 -0500

    closes #10

--- ProblemSets/PS5-ddc/PS5starter.jl
+++ ProblemSets/PS5-ddc/PS5starter.jl
@@ -21,7 +21,7 @@ include("create_grids.jl")
 
 
 # create bus id variable
-df = @transform(df, bus_id = 1:size(df,1))
+df = @transform(df, :bus_id = 1:size(df,1))
 
 #---------------------------------------------------
 # reshape from wide to long (must do this twice be-
@@ -32,14 +32,14 @@ df = @transform(df, bus_id = 1:size(df,1))
 dfy = @select(df, :bus_id,:Y1,:Y2,:Y3,:Y4,:Y5,:Y6,:Y7,:Y8,:Y9,:Y10,:Y11,:Y12,:Y13,:Y14,:Y15,:Y16,:Y17,:Y18,:Y19,:Y20,:RouteUsage,:Branded)
 dfy_long = DataFrames.stack(dfy, Not([:bus_id,:RouteUsage,:Branded]))
 rename!(dfy_long, :value => :Y)
-dfy_long = @transform(dfy_long, time = kron(collect([1:20]...),ones(size(df,1))))
+dfy_long = @transform(dfy_long, :time = kron(collect([1:20]...),ones(size(df,1))))
 select!(dfy_long, Not(:variable))
 
 # next reshape the odometer variable
 dfx = @select(df, :bus_id,:Odo1,:Odo2,:Odo3,:Odo4,:Odo5,:Odo6,:Odo7,:Odo8,:Odo9,:Odo10,:Odo11,:Odo12,:Odo13,:Odo14,:Odo15,:Odo16,:Odo17,:Odo18,:Odo19,:Odo20)
 dfx_long = DataFrames.stack(dfx, Not([:bus_id]))
 rename!(dfx_long, :value => :Odometer)
-dfx_long = @transform(dfx_long, time = kron(collect([1:20]...),ones(size(df,1))))
+dfx_long = @transform(dfx_long, :time = kron(collect([1:20]...),ones(size(df,1))))
 select!(dfx_long, Not(:variable))
 
 # join reshaped df's back together
